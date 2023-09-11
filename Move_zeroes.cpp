8092358b2f45ff1ec9850d84a146281055bed0cc
Shashwat Bajpai <112643512+Bajpai25@users.noreply.github.com>
2023-09-11 17:39:52 +0530

    Create Move_zeroes

---
+++ Move_zeroes.cpp
@@ -0,0 +1,29 @@
+Given an integer array nums, move all 0's to the end of it while maintaining the relative order of the non-zero elements.
+
+Note that you must do this in-place without making a copy of the array.
+
+ 
+
+Example 1:
+
+Input: nums = [0,1,0,3,12]
+Output: [1,3,12,0,0]
+Example 2:
+
+Input: nums = [0]
+Output: [0]
+
+  Code : 2 pointer approach ::
+class Solution {
+public:
+    // 2 pointer approach
+    void moveZeroes(vector<int>& nums) {
+        int i=0;
+        for(int j=0;j<nums.size();j++){
+            if(nums[j]!=0){
+                swap(nums[j],nums[i]);
+                i++;
+            }
+        }
+    }
+};
